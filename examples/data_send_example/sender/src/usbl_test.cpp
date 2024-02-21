2282f3a94b1762e49e3af59c1d5e6277412f9b04
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-02-21 16:46:50 -0700

    cleaned up cid dat send

--- examples/data_send_example/sender/src/usbl_test.cpp
+++ examples/data_send_example/sender/src/usbl_test.cpp
@@ -6,21 +6,6 @@
 using namespace narval::seatrac;
 
 
-
-
-/*
-https://www.seascapesubsea.com/downloads/Blueprint-Subsea-SeaTrac-Developer-Guide.pdf
-Seatrac dev manual page 116
-*/
-// struct CidDatSendMessage {
-//     CID_E msgId;
-//     BID_E destId;
-//     AMSGTYPE_E msgType;
-//     uint8_t packetLen;
-//     uint8_t packetData[31];
-// }__attribute__((packed));
-
-
 class MyDriver : public SeatracDriver
 {
     public:
@@ -29,54 +14,22 @@ class MyDriver : public SeatracDriver
         SeatracDriver(serialPort)
     {}
 
-    void send_data(const uint8_t* data, uint8_t length) {
-        std::cout << "ping_beacon start" << std::endl;
+    void send_data(const BID_E destId, 
+                   const AMSGTYPE_E msgType, 
+                   const uint8_t data_length, 
+                   const uint8_t* data) {
 
-        //create a message packet to send
-        messages::DataSend message;
+        messages::DataSend message;   //create a message packet to send
         
         message.destId = BEACON_ALL;  //send it to all beacons regardless of id
         message.msgType = MSG_OWAYU;  //send the data away to other beacons with usbl information
-        message.packetLen = std::min(length, (uint8_t)31); //31; //the length of data packet
+        message.packetLen = std::min(data_length, (uint8_t)31); //the length of data packet
 
-        //copy the bytes (chars) from data into our message structure
-        std::memcpy(message.packetData, data, std::min(length, (uint8_t)31));
+        std::memcpy(message.packetData, data, message.packetLen); //copy the bytes (chars) from data into our message structure
 
-        //fill the remaining bytes with 0x00. Might not be necessary
-        //std::memset(message.packetData + sizeof(data), 0x00, sizeof(message.packetData) - sizeof(data));
+        this->send(sizeof(message), (const uint8_t*)&message); 
 
-        this->send(sizeof(message), (const uint8_t*)&message);
-
-        std::cout << "Data to be sent:";
-        for(int i=0; i<length; i++) {
-            printf("%x ", data[i]);
-        }
-        printf("\n");
-
-        std::cout << "Message being sent to modem:";
-        uint8_t* msgdata = (uint8_t*)&message;
-        for(int i=0; i<sizeof(message); i++) {
-            printf("%x ", msgdata[i]);
-        }
-        std::cout << std::endl;
-
-
-        std::cout << "ping_beacon end" << std::endl;
     }
-
-    void on_receive(const std::vector<uint8_t>& data) {
-        if (data[0] != CID_STATUS) { // if the data is not a regular status message. Too many status messages
-            //std::cout << sizeof(data) << std::endl;
-            std::cout << "on recieve";
-            for(int i=0; i<sizeof(data); i++) {
-                printf("%x ", data[i]);
-            }
-            std::cout << std::endl;
-        }
-
-        SeatracDriver::on_receive(data);
-    }
-
 
     // this method is called on any message returned by the beacon.
     void on_message(CID_E msgId, const std::vector<uint8_t>& data) {
@@ -113,7 +66,7 @@ class MyDriver : public SeatracDriver
 
 int main(int argc, char *argv[])
 {
-    
+    //set serial to command line input if given
     std::string serial_port;
     if (argc == 1) { serial_port = "/dev/ttyUSB0"; }
     else { serial_port = argv[1]; }
@@ -125,9 +78,14 @@ int main(int argc, char *argv[])
     for (int i=0; i<50; i++) {
         std::cout << "message text: " << std::flush;
         getline(std::cin, message_txt);
-        if (message_txt.length() == 0) { break; }
+        if (message_txt.length() == 0) break;
 
-        seatrac.send_data((const uint8_t*)message_txt.data(), message_txt.size());
+        seatrac.send_data(
+            BEACON_ALL,
+            MSG_OWAYU, //other valid types: MSG_OWAY, MSG_REQ, MSG_REQU, MSG_REQX
+            (uint8_t)message_txt.size(),
+            (const uint8_t*)message_txt.data()
+          );
     }
 
     //getchar();
