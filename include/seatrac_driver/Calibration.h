313d0a3a402c379446c8d0b95f65681d6492f482
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-03-29 12:31:13 -0600

    started working on calibration script

---
+++ include/seatrac_driver/Calibration.h
@@ -0,0 +1,12 @@
+#ifndef _DEF_SEATRAC_DRIVER_CALIBRATION_H_
+#define _DEF_SEATRAC_DRIVER_CALIBRATION_H_
+
+
+namespace narval { namespace seatrac { namespace command {
+
+
+}; //namespace calibration
+}; //namespace seatrac
+}; //namespace narval
+
+#endif //_DEF_SEATRAC_DRIVER_CALIBRATION_H_
