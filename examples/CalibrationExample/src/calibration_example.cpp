973911c7a648688ea5b327210a4198b22cd0b18f
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-05-06 12:48:55 -0600

    calibration feedback display

--- examples/CalibrationExample/src/calibration_example.cpp
+++ examples/CalibrationExample/src/calibration_example.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
-#include <stdio.h>
 
+#include <seatrac_driver/SeatracDriver.h>
+#include <seatrac_driver/messages/Messages.h>
+#include <seatrac_driver/commands.h>
 #include <seatrac_driver/Calibration.h>
 using namespace narval::seatrac;
 
+
+class MyDriver : public SeatracDriver
+{
+    public:
+
+    MyDriver(const std::string& serialPort = "/dev/ttyUSB0") :
+        SeatracDriver(serialPort)
+    {}
+
+    // this method is called on any message returned by the beacon.
+    void on_message(CID_E msgId, const std::vector<uint8_t>& data) {
+        switch(msgId) {
+            default:
+                std::cout << "Got message : " << msgId << std::endl << std::flush;
+                break;
+
+            case CID_STATUS: {
+                messages::Status status;
+                status = data;
+                calibration::printCalFeedback(std::cout, status);
+            } break;
+        }
+    }
+};
+
 int main(int argc, char *argv[])
 {
-    calibration::calibrateAccelerometer();
-    calibration::calibrateMagnetometer();
+    //set serial to command line input if given
+    std::string serial_port;
+    if (argc == 1) { serial_port = "/dev/ttyUSB0"; }
+    else { serial_port = argv[1]; }
+
+    MyDriver seatrac(serial_port);
+
+    calibration::turnOnAccCalFeedback(seatrac);
+    getchar();
+    calibration::turnOffCalFeedback(seatrac);
+    calibration::turnOnMagCalFeedback(seatrac);
+    getchar();
+    calibration::turnOffCalFeedback(seatrac);
+
     return 0;
 }
--- include/seatrac_driver/Calibration.h
+++ include/seatrac_driver/Calibration.h
@@ -27,74 +27,79 @@ namespace narval { namespace seatrac { namespace calibration {
         CST_E status;
     }__attribute__((packet));
 
+    inline void turnOnAccCalFeedback(SeatracDriver& seatrac, STATUS_BITS_E prevStatusBits=static_cast<STATUS_BITS_E>(0x0), STATUSMODE_E statusMode=STATUS_MODE_10HZ) {
+        messages::StatusConfigSet::Request statusSet;
+        
+        statusSet.statusOutput = ACC_CAL | AHRS_RAW_DATA | prevStatusBits; 
+        statusSet.statusMode   = statusMode;
 
-    class CalibrationDriver: public SeatracDriver  {
-    public:
+        seatrac.send(sizeof(statusSet), (const uint8_t*)&statusSet);
+    }
+    inline void turnOnMagCalFeedback(SeatracDriver& seatrac, STATUS_BITS_E prevStatusBits=static_cast<STATUS_BITS_E>(0x0), STATUSMODE_E statusMode=STATUS_MODE_10HZ) {
+        messages::StatusConfigSet::Request statusSet;
+        
+        statusSet.statusOutput = MAG_CAL | AHRS_RAW_DATA | prevStatusBits; 
+        statusSet.statusMode   = statusMode;
 
-        CalibrationDriver(std::ostream& out, const std::string& serialPort = "/dev/ttyUSB0") :
-            SeatracDriver(serialPort)
-        {
-            print_out = &out;
+        seatrac.send(sizeof(statusSet), (const uint8_t*)&statusSet);
+    }
+    inline void turnOffCalFeedback(SeatracDriver& seatrac, STATUS_BITS_E prevStatusBits=static_cast<STATUS_BITS_E>(0x0), STATUSMODE_E statusMode=STATUS_MODE_10HZ) {
+        messages::StatusConfigSet::Request statusSet;
+        
+        statusSet.statusOutput = static_cast<STATUS_BITS_E>(prevStatusBits & ~ACC_CAL & ~MAG_CAL & ~AHRS_RAW_DATA); 
+        statusSet.statusMode   = statusMode;
+
+        seatrac.send(sizeof(statusSet), (const uint8_t*)&statusSet);
+    }
+
+    //call this function in SeatracDriver::on_message under the CID_STATUS case to
+    //print calibration data if calibration fields exist
+
+    inline void printCalFeedback(std::ostream& out, const messages::Status& status) {
+        if (status.contentType & ACC_CAL) {
+            out << "\tXmin:" << status.accCalibration.accLimMinX
+                << "\tXmax:" << status.accCalibration.accLimMaxX
+                << "\tYmin:" << status.accCalibration.accLimMinY
+                << "\tYmax:" << status.accCalibration.accLimMaxY
+                << "\tZmin:" << status.accCalibration.accLimMinZ
+                << "\tZmax:" << status.accCalibration.accLimMaxZ
+                << std::endl;
         }
-
-        // this method is called on any message returned by the beacon.
-        void on_message(CID_E msgId, const std::vector<uint8_t>& data) {
-            //replace code in this method by your own
-            switch(msgId) {
-                default:
-                    std::cout << "Got message : " << msgId << std::endl << std::flush;
-                    break;
-
-                case CID_STATUS: {
-                        messages::Status status;
-                        status = data;
-                        if (status.contentType & ACC_CAL) {
-                            status.accCalibration
-                        }
-                        if (status.contentType & MAG_CAL) {
-                            status.magCalibration
-                        }
-                    } 
-                    break;
-            }
+        else if (status.contentType & MAG_CAL) {
+            uint8_t mag_progress = status.magCalibration.magCalBuf;
+            std::string progressBar(' ',50);
+            for(uint8_t i=0; i<mag_progress/2; i++) progressBar[i] = '=';
+            out << "MagCal: " << mag_progress << "%\t" 
+                << "|" << progressBar << "|" << std::endl;
         }
+    }
 
-        //Call this function to run a calibration command
-        //Function walks user through a terminal calibration procedure
-        //Take note that this is a blocking function
-        bool calibrateAccelerometer(std::ostream& out, std::istream& in, std::string serial_port) {
+    
+    //Call this function to run a calibration command
+    //Function walks user through a terminal calibration procedure
+    //Take note that this is a blocking function
+    bool calibrateAccelerometer(SeatracDriver& seatrac, std::ostream& out, std::istream& in) {
 
-            messages::StatusConfigSet::Request statusSet;
-            statusSet.statusOutput = STATUS_BITS_E::ACC_CAL + STATUS_BITS_E::AHRS_RAW_DATA; //status set to report the accelerometer calibration
+        char input[50];
+        out << "--- Seatrac USBL Modem Accelerometer Calibration ---" << std::endl
+            << "This calibration procedure may be performed out of water" //TODO: check if true
+            << "Please hold the modem in an upright position (with the modem cable facing down)" << std::endl
+            << "When ready, Press Enter to continue";
+        in.get();
+        out << "Slowly rotate the beacon around the verticle axis"
+        //TODO: finish calibration sequence
+    }
 
+    bool calibrateMagnetometer(std::ostream& out, std::istream& in, std::string serial_port) {
 
-            char input[50];
-            out << "--- Seatrac USBL Modem Accelerometer Calibration ---" << std::endl
-                << "This calibration procedure may be performed out of water" //TODO: check if true
-                << "Please hold the modem in an upright position (with the modem cable facing down)" << std::endl
-                << "When ready, Press Enter to continue";
-            in.get();
-            out << "Slowly rotate the beacon around the verticle axis"
-            //TODO: finish calibration sequence
-        }
-
-        bool calibrateMagnetometer(std::ostream& out, std::istream& in, std::string serial_port) {
-
-            CalibrationDriver calSeatrac(serial_port);
-
-
-            char input[50];
-            out << "--- Seatrac USBL Modem Magnetometer Calibration ---" << std::endl
-                << "This calibration procedure may be performed out of water" //TODO: check if true
-                << "Please hold the modem in an upright position (with the modem cable facing down)" << std::endl
-                << "When ready, Press Enter to continue";
-            in.get();
-            out << "Slowly rotate the beacon around the verticle axis"
-            //TODO: finish calibration sequence
-        }
-
-    private:
-        std::ostream* print_out;
+        char input[50];
+        out << "--- Seatrac USBL Modem Magnetometer Calibration ---" << std::endl
+            << "This calibration procedure may be performed out of water" //TODO: check if true
+            << "Please hold the modem in an upright position (with the modem cable facing down)" << std::endl
+            << "When ready, Press Enter to continue";
+        in.get();
+        out << "Slowly rotate the beacon around the verticle axis"
+        //TODO: finish calibration sequence
     }
 
 }; //namespace calibration
